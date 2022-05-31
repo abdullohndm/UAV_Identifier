32acfdafe37bf248bba06a1360a94f99bc0845de
Abdullah N <abdullahloveb@gmail.com>
2022-05-31 14:18:37 +0700

    50%

--- README.md
+++
@@ -1,2 +0,0 @@
-# UAV Identifier
-Recognizing and Identifying Unmanned Aerial Vehicle System using Machine Learning for Classification
---
+++ main.py
@@ -0,0 +1,173 @@
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify, flash
+from flask_mysqldb import MySQL, MySQLdb
+import speech_recognition as sr
+import MySQLdb.cursors
+import re
+
+app = Flask(__name__)
+
+app.secret_key = '123'
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'geeklogin'
+
+mysql = MySQL(app)
+mysql.init_app(app)
+
+@app.route('/')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    msg=''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password))
+        account = cursor.fetchone()
+        if account:
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            return redirect(url_for('home'))
+        else:
+            msg("Incorrect username/password!", "danger")
+    return render_template('login.html',title="Login")
+
+@app.route('/logout')
+def logout():
+	session.pop('loggedin', None)
+	session.pop('id', None)
+	session.pop('username', None)
+	return redirect(url_for('login'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+    elif request.method == 'POST':
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = %s', (username,))
+        account = cursor.fetchone()
+        if account:
+            msg = 'Account already exists!'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address!'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers!'
+        elif not username or not password or not email:
+            msg = 'Please fill out the form!'
+        else:
+            cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s)', (username, password, email,))
+            mysql.connection.commit()
+            msg = 'You have successfully registered!'
+    return render_template('register.html', msg=msg)
+
+@app.route('/home')
+def home():
+    if 'loggedin' in session:
+        return render_template('home.html', username=session['username'])
+    return redirect(url_for('login'))
+	
+@app.route('/profile')
+def profile():
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    # Check if user is loggedin
+    if 'loggedin' in session:
+        cursor.execute('SELECT * FROM accounts WHERE id = %s', [session['id']])
+        account = cursor.fetchone()
+        # Show the profile page with account info
+        return render_template('profile.html', account=account)
+    # User is not loggedin redirect to login page
+    return redirect(url_for('login'))
+
+@app.route('/coba1')
+def coba1():
+    transcript = ""
+    if request.method == "POST":
+        print("FORM DATA RECEIVED")
+        if "file" not in request.files:
+            return redirect(request.url)
+        file = request.files["file"]
+        if file.filename == "":
+            return redirect(request.url)
+        if file:
+            recognizer = sr.Recognizer()
+            audioFile = sr.AudioFile(file)
+            with audioFile as source:
+                data = recognizer.record(source)
+            transcript = recognizer.recognize_google(data, key=None, language="id-ID")
+    return render_template('coba1.html', transcript=transcript)
+
+@app.route('/input')
+def input():
+   return render_template('input.html')
+
+@app.route('/testing')
+def testing():
+   return render_template('testing.html')
+
+@app.route('/dataset',methods = ['POST', 'GET'])
+def dataset():
+    cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    result = cur.execute("SELECT * FROM tblemployee ORDER BY id")
+    employee = cur.fetchall()
+    return render_template('dataset.html', employee=employee)
+ 
+@app.route("/ajax_add",methods=["POST","GET"])
+def ajax_add():
+    cursor = mysql.connection.cursor()
+    cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    if request.method == 'POST':
+        txtname = request.form['txtname']
+        txtdepartment = request.form['txtdepartment']
+        txtphone = request.form['txtphone']
+        print(txtname)
+        if txtname == '':
+            msg = 'Please Input name'  
+        elif txtdepartment == '':
+           msg = 'Please Input Department'  
+        elif txtphone == '':
+           msg = 'Please Input Phone'  
+        else:        
+            cur.execute("INSERT INTO tblemployee (name,department,phone) VALUES (%s,%s,%s)",[txtname,txtdepartment,txtphone])
+            mysql.connection.commit()       
+            cur.close()
+            msg = 'New record created successfully'   
+    return jsonify(msg)
+ 
+@app.route("/ajax_update",methods=["POST","GET"])
+def ajax_update():
+    cursor = mysql.connection.cursor()
+    cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    if request.method == 'POST':
+        string = request.form['string']
+        txtname = request.form['txtname']
+        txtdepartment = request.form['txtdepartment']
+        txtphone = request.form['txtphone']
+        print(string)
+        cur.execute("UPDATE tblemployee SET name = %s, department = %s, phone = %s WHERE id = %s ", [txtname, txtdepartment, txtphone, string])
+        mysql.connection.commit()       
+        cur.close()
+        msg = 'Record successfully Updated'   
+    return jsonify(msg)    
+ 
+@app.route("/ajax_delete",methods=["POST","GET"])
+def ajax_delete():
+    cursor = mysql.connection.cursor()
+    cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    if request.method == 'POST':
+        getid = request.form['string']
+        print(getid)
+        cur.execute('DELETE FROM tblemployee WHERE id = {0}'.format(getid))
+        mysql.connection.commit()       
+        cur.close()
+        msg = 'Record deleted successfully'   
+    return jsonify(msg) 
+
+if __name__ == '__main__':
+    app.run(debug=True)
